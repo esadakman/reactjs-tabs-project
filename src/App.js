89ed0eb9fec81b74e5b3d2c523ba27d4b8b42074
esadakman <esadd26@gmail.com>
2022-07-05 15:45:05 +0300

    v2.0

--- src/App.js
+++ src/App.js
@@ -1,27 +1,74 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FaAngleDoubleRight } from "react-icons/fa";
 
 const API_URL = "https://course-api.com/react-tabs-project";
 
 const App = () => {
-  const [data, setData] = useState([]);
-
+  const [apiData, setApiData] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [value, setValue] = useState(0);
   const fetchData = async () => {
     try {
       const response = await fetch(API_URL);
       const data = await response.json();
       console.log(data);
+      setApiData(data);
+      setLoading(false);
     } catch (error) {
-      // console.log(error);
+      console.log(error);
     }
   };
 
-  fetchData();
+  useEffect(() => {
+    fetchData();
+  }, []);
 
+  if (isLoading) {
+    return (
+      <section className="section loading">
+        <h1>Loading ...</h1>
+      </section>
+    );
+  }
+
+  const { company, dates, duties, title } = apiData[value];
+
+  // console.log(company);
   return (
-    <div>
-      <FaAngleDoubleRight />
-    </div>
+    <section className="section">
+      <div className="title">
+        <h2>Experience</h2>
+      </div>
+      <div className="underline"></div>
+      <div className="jobs-center">
+        <div className="btn-container">
+          {apiData.map((item, index) => {
+            return (
+              <button
+                key={index}
+                onClick={() => setValue(index)}
+                className={`job-btn ${index === value && "active-btn"}`}
+              >
+                {item.company}
+              </button>
+            );
+          })}
+        </div>
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date"> {dates} </p>
+          {duties.map((item, index) => {
+            return (
+              <div key={index} className="job-desc">
+                <FaAngleDoubleRight />
+                <p>{item}</p>
+              </div>
+            );
+          })}
+        </article>
+      </div>
+    </section>
   );
 };
 
